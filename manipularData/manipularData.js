9bbfc9ad9457e023bc9666ba0410d58d282f2418
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-18 15:03:47 -0300

    Added moment.js import, reformatted console logs with comments

--- manipularData/manipularData.js
+++ manipularData/manipularData.js
@@ -1,13 +1,17 @@
+const moment = require('moment');
 let data = moment();
 
 // Adicionar dias
 data.add(7, 'days');
-console.log(data.format("DD/MM/YYYY")); // Ex: 25/11/2024
+console.log(data.format("DD/MM/YYYY"));
+// Ex: 25/11/2024
 
 // Subtrair meses
 data.subtract(1, 'month');
-console.log(data.format("DD/MM/YYYY")); // Ex: 25/10/2024
+console.log(data.format("DD/MM/YYYY"));
+// Ex: 25/10/2024
 
 // Modificar ano, mês, etc.
 data.year(2025).month(0).date(1);
-console.log(data.format("DD/MM/YYYY")); // Ex: 01/01/2025
+console.log(data.format("DD/MM/YYYY"));
+// Ex: 01/01/2025
