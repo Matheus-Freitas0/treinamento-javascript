af5682cd72d5cfa140e57ae7103176a4a107fdff
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-18 16:02:58 -0300

    date between has been implemented

---
+++ manipularData/entreDatas.js
@@ -0,0 +1,12 @@
+const moment = require('moment');
+
+const dataInicial = moment("2024-11-01");
+const dataFinal = moment("2024-11-30");
+const dataTeste = moment("2024-10-18");
+
+if (dataTeste.isBetween(dataInicial, dataFinal)) {
+  console.log("A data está entre as duas datas!");
+}
+else {
+  console.log("A data não está entre as duas datas.");
+}
